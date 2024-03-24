2ac9c7e82d830bd89bc0103f6c0b39b5422d7a46
Subhasish-2005 <subhasish2005jana@gmail.com>
2024-03-24 16:33:03 +0530

    prg5 week_1

---
+++ prg5.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    float r;
+    cout<<"enter radius of circle: ";
+    cin>>r;
+    cout<<"the diameter of the circle is: ";
+    cout<<2*r<<endl;
+    cout<<"the circumference of the circle is: ";
+    cout<<2*3.142*r<<endl;
+    cout<<"the area of the circle is: ";
+    cout<<3.142*r*r<<endl;
+    return 0;
+}
